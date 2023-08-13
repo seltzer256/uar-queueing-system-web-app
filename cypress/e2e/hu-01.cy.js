a9d7e541d70f54b90eba89b7ad44c152093a9318
henryaftershock <henry256@hotmail.es>
2023-08-13 16:03:54 -0500

    fix: prettier

--- cypress/e2e/hu-01.cy.js
+++ cypress/e2e/hu-01.cy.js
@@ -1,35 +1,41 @@
 describe("Registro y autenticación de usuarios", () => {
   it("Ingreso al sistema", () => {
     cy.visit("/login");
-    cy.get('#login-email').type("john@email.com");
-    cy.get('#login-password').type("123123123");
-    cy.get('#login-btn').click();
+    cy.get("#login-email").type("john@email.com");
+    cy.get("#login-password").type("123123123");
+    cy.get("#login-btn").click();
     cy.url().should("eq", "http://localhost:3000/dashboard/");
   });
 
   it("Crear usuario", () => {
     cy.visit("/login");
-    cy.get('#login-email').type("john@email.com");
-    cy.get('#login-password').type("123123123");
-    cy.get('#login-btn').click();
+    cy.get("#login-email").type("john@email.com");
+    cy.get("#login-password").type("123123123");
+    cy.get("#login-btn").click();
     cy.get('[data-test="tab-users"]').click();
     cy.get('[data-test="user-name"]').type("Test Name");
     cy.get('[data-test="user-email"]').type("test@email.com");
     cy.get('[data-test="user-password"]').type("123123123");
     cy.get('[data-test="user-btn-save"]').click();
-    cy.get('.Toastify__toast-body > :nth-child(2)').should("have.text", "Usuario creado correctamente");
-  })
+    cy.get(".Toastify__toast-body > :nth-child(2)").should(
+      "have.text",
+      "Usuario creado correctamente"
+    );
+  });
 
   it("Actualizar información", () => {
     cy.visit("/login");
-    cy.get('#login-email').type("john@email.com");
-    cy.get('#login-password').type("123123123");
-    cy.get('#login-btn').click();
+    cy.get("#login-email").type("john@email.com");
+    cy.get("#login-password").type("123123123");
+    cy.get("#login-btn").click();
     cy.get('[data-test="menu-btn"]').click();
     cy.get('[data-test="menu-profile"]').click();
-    cy.get('[data-test="profile-name"]').clear()
+    cy.get('[data-test="profile-name"]').clear();
     cy.get('[data-test="profile-name"]').type("Test Name 2");
     cy.get('[data-test="profile-btn-save"]').click();
-    cy.get('.Toastify__toast-body > :nth-child(2)').should("have.text", "Información actualizada!");
-  })
+    cy.get(".Toastify__toast-body > :nth-child(2)").should(
+      "have.text",
+      "Información actualizada!"
+    );
+  });
 });
